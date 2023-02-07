17c82c829270446a75716993e2f928dfaa6af76a
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-07 13:28:26 +0300

    03_reverseString all tests passed

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = '';
+    let length = string.length - 1;
+    for (i=length; i>=0; i--){
+        reversed = reversed + string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
