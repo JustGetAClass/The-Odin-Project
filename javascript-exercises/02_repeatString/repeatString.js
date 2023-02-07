eb20fe97b0f2c5aea4ffa9284d8ca97c7f19ddb7
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-07 13:15:06 +0300

    02_repeatString all tests passed

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = '';
+    if (num < 0){
+        repeated = 'ERROR';
+    }
+    for(let i=0; i<num; i++){
+        repeated = repeated + string;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
