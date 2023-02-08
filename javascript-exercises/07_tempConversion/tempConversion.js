b9933a40af38503fab789dee3b9a3b43bfbc9b59
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-08 11:17:01 +0300

    07_tempConversion all tests passed

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = (temp - 32) * 5/9;
+  return decimalPlace(celcius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = temp * (9/5) + 32;
+  return decimalPlace(fahrenheit);
 };
+
+function decimalPlace (number){
+  return Number((Math.round(number * 10) / 10).toFixed(1));
+}
 
 // Do not edit below this line
 module.exports = {
