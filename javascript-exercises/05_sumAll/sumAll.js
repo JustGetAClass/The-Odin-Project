bf91ca74338a65f089d758f9b9b97ece70abf803
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-08 10:12:48 +0300

    05_sumAll all tests passed

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if ((!Number.isInteger(min) || (!Number.isInteger(max)))) {
+        return 'ERROR';
+    }
+    if ((min < 0) || (max < 0)) {
+        return 'ERROR';
+    }
+    if (min > max) {
+        let temp = max;
+        max = min;
+        min = temp;
+    }
+    let sum = 0;
+    for (let index = min; index <= max; index++) {
+        sum = sum + index;
+    }
+    return sum;
 };
 
 // Do not edit below this line
