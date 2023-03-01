f7268d858113e878398f6e4a1ee6c6b943eda01c
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-01 17:16:18 +0300

    08_calculator done

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((mult, item) => mult * item);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	let product = 1;
+	for (let i = 1; i <= num; i++) {
+		product *= i;
+	}
+	return product;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
