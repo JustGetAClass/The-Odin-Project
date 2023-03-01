312a9ec980dbf484c86b1d1c7a861652269d1d9a
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-01 18:09:22 +0300

    09_palindromes done. learned some regex

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processed = str.toLowerCase().replace(/[^a-z]/g, "");
+    return (processed.split("").reverse().join("") === processed);
 };
 
 // Do not edit below this line
